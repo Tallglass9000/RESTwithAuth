33681886ab34280a2c1453bc4ad1fb4205a18db5
Monica Wu <shoprandom99@yahoo.com>
2015-09-29 11:48:12 -0700

    Update users_routes.js

--- routes/users_routes.js
+++ routes/users_routes.js
@@ -8,24 +8,22 @@ var ee = new EventEmitter();
 
 var usersRouter = module.exports = exports = express.Router();
 
-usersRouter.post('/signup', jsonParser, function(req, res) {
+usersRouter.post('/signup', jsonParser, function (req, res) {
   var newUser = new User();
   newUser.basic.username = req.body.username;
   newUser.username = req.body.username;
   ee.emit('firstAction', req, res, newUser);
 });
 
-
-
-ee.on('firstAction', function(req, res, newUser) {
-  newUser.generateHash(req.body.password, function(err, hash) {
+ee.on('firstAction', function (req, res, newUser) {
+  newUser.generateHash(req.body.password, function (err, hash) {
     if (err) return handleError(err, res);
     ee.emit('secondAction', req, res, newUser);
   });
 });
 
-ee.on('secondAction', function(req, res, newUser) {
-  newUser.save(function(err, data) {
+ee.on('secondAction', function (req, res, newUser) {
+  newUser.save(function (err, data) {
     if (err) return handleError(err, res);
     newUser.generateToken(function(err, token) {
       if (err) return handleError(err, res);
@@ -34,33 +32,31 @@ ee.on('secondAction', function(req, res, newUser) {
   });
 });
 
-
-
-
-
-
-
-
-usersRouter.get('/signin', httpBasic, function(req, res) {
-  User.findOne({'basic.username': req.auth.username}, function(err, user) {
+usersRouter.get('/signin', httpBasic, function (req, res) {
+  User.findOne({'basic.username': req.auth.username}, function (err, user) {
     if (err) return handleError(err, res);
-
     if (!user) {
       console.log('could not authenticat: ' + req.auth.username);
       return res.status(401).json({msg: 'could not authenticat'});
+      ee.emit('compareHash', req, res, user);
     }
+  });
+});
 
-    user.compareHash(req.auth.password, function(err, hashRes) {
-      if (err) return handleError(err, res);
-      if (!hashRes) {
-        console.log('could not authenticat: ' + req.auth.username);
-        return res.status(401).json({msg: 'authenticat says no!'});
-      }
+ee.on('compareHash', function (req, res, user) {
+  user.compareHash(req.auth.password, function (err, hashRes) {
+    if (err) return handleError(err, res);
+    if (!hashRes) {
+      console.log('could not authenticat: ' + req.auth.username);
+      return res.status(401).json({msg: 'authenticat says no!'});
+      ee.emit('generateToken', req, res, user);
+    }
+  });
+});
 
-      user.generateToken(function(err, token) {
-        if (err) return handleError(err, res);
-        res.json({token: token});
-      });
-    });
+ee.on('generateToken', function (req, res, user) {
+  user.generateToken(function (err, token) {
+    if (err) return handleError(err, res);
+    res.json({token: token});
   });
 });
